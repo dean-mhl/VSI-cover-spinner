7c0477bfd82d0ec87418788eed0e9373cc882285
dean-mhl <dbaumeister@mhl.org>
2021-11-18 16:58:06 -0500

    update js/index.js to include config preferences

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
 import Slot from "./Slot.js";
+import WarpSpeed from "./warpspeed.js";
 
 const config = {
   inverted: false, // true: reels spin from top to bottom; false: reels spin from bottom to top
 };
 
 const slot = new Slot(document.getElementById("slot"), config);
+
+const x = new WarpSpeed("canvas",'{"speed":25,"targetSpeed":2,"speedAdjFactor":0.01,"density":0.7,"shape":"square","warpEffect":true,"warpEffectLength":5,"depthFade":false,"starSize":3,"backgroundColor":"#000000","starColor":"#FFFFFF"}'
+);
+
+
