0e10bfba3e24c885d8e033abac3254736805dfdd
dean-mhl <dbaumeister@mhl.org>
2021-11-22 14:11:07 -0500

    make webpack.config.js work with latest version of npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: ["./src/js/index.js", "./src/css/style.css"],
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
   },
   performance: {
     hints: false,
@@ -19,30 +18,19 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {name: '[name].[ext]'}
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {name: '[name].[ext]'}
-          }
-        ]
+        type: "asset/resource",
       },
       {
         test: /\.js$/,
